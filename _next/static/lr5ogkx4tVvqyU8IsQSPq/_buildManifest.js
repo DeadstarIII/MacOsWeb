a5a996ea909e59dbd0e18ebca5d88ec87b0a0677
DeadstarIII <74278992+DeadstarIII@users.noreply.github.com>
2022-08-21 17:56:06 +0000

    Deploying to gh-pages from  @ c683754325cfd0a475aa6b6b67eecd21ecfb5a04 🚀

---
+++ _next/static/lr5ogkx4tVvqyU8IsQSPq/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f8dbaca22d8874fb.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/dock":["static/chunks/pages/dock-6e5f04620301686a.js"],sortedPages:["/","/_app","/_error","/dock"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
