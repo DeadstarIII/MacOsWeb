b590bc6eea875dba8959065c422847831adc56f1
deadstar <74278992+DeadstarIII@users.noreply.github.com>
2022-08-25 23:53:49 +0530

    Initial commit

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,10 @@
 import Dock from "./Dock.jsx";
+import Window from "./Window.jsx";
 const Layout = ({ children }) => {
   return (
     <div>
       <Dock />
+      <Window />
       {children}
     </div>
   );
